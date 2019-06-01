4fa730a761bc0f9e9790fac1a0a20bb35bcfe84b
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-06-02 01:43:59 +0530

    :sparkles: Added lazy loading with react lazy and suspense

--- src/Routes.js
+++ src/Routes.js
@@ -1,54 +1,73 @@
-import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import App from './App';
-import Blogs from './Blogs';
-import Contact from './Contact';
-import BlogPost from './BlogPost';
-import PrivateRoute from './PrivateRoute';
-import AddBlog from './AddBlog';
-import config from './Firebase';
-import Login from './Login';
-import Search from './Search';
-import { AtomSpinner } from 'react-epic-spinners';
+import React, { lazy, Suspense, useEffect, useState, useReducer } from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import App from './App'
+import config from './Firebase'
+import PrivateRoute from './PrivateRoute'
+import { AtomSpinner, FulfillingSquareSpinner } from 'react-epic-spinners'
 
-export default class Routes extends React.Component{
-    state = { loading: true, authenticated: false, user: null };
+const Blogs = lazy(() => import('./Blogs'))
+const Contact = lazy(() => import('./Contact'))
+const BlogPost = lazy(() => import('./BlogPost'))
+const AddBlog = lazy(() => import('./AddBlog'))
+const Login = lazy(() => import('./Login'))
+const Search = lazy(() => import('./Search'))
 
-    componentWillMount() {
-        config.auth().onAuthStateChanged(user => {
-          if (user) {
-            this.setState({
-              authenticated: true,
-              currentUser: user,
-              loading: false
-            });
-          } else {
-            this.setState({
-              authenticated: false,
-              currentUser: null,
-              loading: false
-            });
-          }
-        });
+const initialState = { loading: true, authenticated: false, currentUser: null }
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'loading':
+      return {
+        ...state,
+        loading: action.loading
+      }
+    case 'authenticated': 
+      return {
+        loading: false,
+        authenticated: true,
+        currentUser: action.user
+      }
+    default:
+      return state
+  }
+}
+
+export default function Routes (props) {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    console.log(state)
+
+    useEffect(() => {
+      dispatch({type: 'loading', loading: true})
+      console.log('ran effect')
+      config.auth().onAuthStateChanged(user => {
+        if(user) 
+          dispatch({
+            type: 'authenticated',
+            user
+          })
+        else 
+          dispatch({type: 'loading', loading: false})
+      })
+    }, [])   
+
+    const { authenticated, loading } = state
+    if (loading) {
+        return <AtomSpinner className="loading" color="#22d5c3" />;
     }
-
-    render(){
-        const { authenticated, loading } = this.state;
-        if (loading) {
-            return <AtomSpinner className="loading" color="#22d5c3" />;
-        }
-        return(
-            <Router>
-                <React.Fragment>
-                    <Route exact path="/" component={App} />
-                    <Route exact path="/blog" component={Blogs} />
-                    <Route exact path="/contact" component={Contact} />
-                    <Route path="/blog/:path" component={BlogPost} />
-                    <PrivateRoute exact path="/yote" component={AddBlog} authenticated={authenticated}/>
-                    <Route exact path="/yeet" component={Login} />
-                    <Route path="/search" component={Search} />
-                </React.Fragment>
-            </Router>
-        )        
-    }
+        
+    return(
+      <Suspense fallback={loading}>
+        <Router>
+          <React.Fragment>
+              <Route exact path="/" component={App} />
+              <Route exact path="/blog" component={(props) => <Blogs {...props} />} />
+              <Route exact path="/contact" component={() => <Contact />} />
+              <Route path="/blog/:path" component={(props) => <BlogPost {...props}/>} />
+              <PrivateRoute exact path="/yote" component={AddBlog} authenticated={authenticated}/>
+              <Route exact path="/yeet" component={() => <Login />} />
+              <Route path="/search" component={() => <Search />} />
+          </React.Fragment>
+        </Router>
+      </Suspense>
+    )      
 }
