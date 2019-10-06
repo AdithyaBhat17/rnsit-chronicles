ff2039c285dacd4084475956904a6c6f32b9bd3c
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-10-06 19:39:42 +0530

    :wrench: controlled component and hooks

--- src/Login.js
+++ src/Login.js
@@ -1,49 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
 import config from './Firebase';
 import login from './assets/login.svg';
 
-class Login extends Component {
-    logIn = event => {
+const Login = props => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const logIn = (event) => {
         event.preventDefault();
-        const { email, password } = event.target.elements;
         try {
           config
             .auth()
-            .signInWithEmailAndPassword(email.value, password.value);
-          this.props.history.push('/yote');
+            .signInWithEmailAndPassword(email, password);
+          props.history.push('/yote');
         } catch (error) {
           alert(error);
         }
-    };
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 col-sm-12">
-                            <form onSubmit={this.logIn} className="form loginform">
-                                <div className="form-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input type="email" name="email" required className="form-control loginbar" placeholder="shrek@gmail.com"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password">Password</label>
-                                    <input type="password" required name="password" className="form-control loginbar" placeholder="*******"/>
-                                </div>
-                                <input type="submit" value="LOGIN"/>
-                            </form>
-                        </div>
-                        <div className="col-md-6 col-sm-12">
-                            <img src={login} alt="login" className="hero-img"/>
-                        </div>
-                    </div>
-                    
-                </div>                
-            </div>
-        );
     }
+
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 col-sm-12">
+                        <form onSubmit={logIn} className="form loginform">
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                 type="email" 
+                                 name="email" 
+                                 required 
+                                 onChange={(e) => setEmail(e.target.value)}
+                                 className="form-control 
+                                 loginbar" placeholder="shrek@gmail.com"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input
+                                 type="password" 
+                                 required 
+                                 name="password"                                  
+                                 onChange={(e) => setPassword(e.target.value)}
+                                 className="form-control loginbar" 
+                                 placeholder="*******"/>
+                            </div>
+                            <input type="submit" value="LOGIN"/>
+                        </form>
+                    </div>
+                    <div className="col-md-6 col-sm-12">
+                        <img src={login} alt="login" className="hero-img"/>
+                    </div>
+                </div>
+                
+            </div>                
+        </div>
+    )
 }
 
 export default Login;
